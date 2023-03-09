dc2d92b5a4f6ae066c2fe0e38dec2f126927af8a
Sid Narayanan <iamisidharth@gmail.com>
2023-03-09 09:07:25 +0530

    New - Added Unit Test cases

---
+++ src/__tests__/json-mapper/handler.test.ts
@@ -0,0 +1,44 @@
+const { Handler } = require('../../json-mapper')
+
+import validData from '../fixtures/json-mapper/valid.json';
+import validNotArrayData from '../fixtures/json-mapper/valid-without-array.json';
+import invalidData from '../fixtures/json-mapper/invalid.json';
+import successResponse from '../fixtures/json-mapper/success-response.json';
+
+describe('JSON Parser Error Handling:::', () => {
+  it('ERROR:invalid payload', async () => {
+    const handler = new Handler()
+
+    const response = await handler.mapper(invalidData);
+
+    expect(response).toStrictEqual({
+        "error": true,
+        "errorCode": 400,
+        "details": "\"referenceData\" is required",
+        "category": "INVALID-INPUT"
+      });
+  });
+
+  it('SUCCESS: Parse data', async () => {
+    const handler = new Handler
+
+    const response = await handler.mapper(validData);
+
+    expect(response).toStrictEqual(successResponse);
+  });
+
+  it('SUCCESS: Parse data without array', async () => {
+    const handler = new Handler
+
+    const response = await handler.mapper(validNotArrayData);
+
+    expect(response).toStrictEqual({
+        "data": {
+          "name": "testIdValue",
+          "valueType": "string",
+          "value": "sip:50002312344314@ims.mod-connect.com"
+        },
+        "error": false
+      });
+  });
+});
